e22bfb1a1c370bd256c5a7dcd892ab6f830caf26
Chris <schemryj@gmail.com>
2016-07-26 16:12:48 +0900

    proptypes 처리 및 문서 추가

--- 02-react/app/components/CheckList.js
+++ 02-react/app/components/CheckList.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 
 class CheckList extends Component {
     render() {
@@ -19,6 +19,11 @@ class CheckList extends Component {
             </div>
         );
     }
-}
+};
+
+CheckList.propTypes = {
+    cardId : PropTypes.number,
+    tasks : PropTypes.arrayOf(PropTypes.object)
+};
 
 export default CheckList;
